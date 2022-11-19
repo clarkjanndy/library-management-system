e86d470cfa76cc6addcf07da888415e3a201d63a
clarkjanndy <cjcasol22@gmail.com>
2022-11-19 16:44:09 +0800

    add dashboard analytics

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import action_views, validation_views
+from .views import action_views, validation_views, analytics_views
 
 urlpatterns = [
-    
+    #validations
     path('validate-id-no', validation_views.validate_id_no, name="validate-id-no"),
     path('validate-barcode', validation_views.validate_barcode, name="validate_barcode"),
+    
+    #analytics
+    path('get-student-count', analytics_views.get_student_count, name="get_student_count"),
+    path('get-teacher-count', analytics_views.get_teacher_count, name="get_teacher_count"),
+    path('get-book-count', analytics_views.get_book_count, name="get_book_count"),
+    
+    path('visit-histogram', analytics_views.visit_histogram, name="visit_histogram"),
 ]
