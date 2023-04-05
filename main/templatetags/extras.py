a4222495549cb07c9c07c99a69f00b0b83239ecd
clarkjanndy <cjcasol22@gmail.com>
2023-04-05 22:31:03 +0800

    add filter on course and fix no book on profile

--- main/templatetags/extras.py
+++ main/templatetags/extras.py
@@ -13,3 +13,18 @@ def cardinal(value):
         return '3rd'
     else:
         return f"{value}th"
+    
+@register.filter()
+def abbreviate(value):
+    if value == 'Bachelor of Science in Information Technology':
+        return 'BSIT'
+    elif value == 'Bachelor of Science in Hospitality Management':
+        return 'BSHM'
+    elif value == 'Bachelor of Science in Criminology':
+        return 'BSCrim'
+    elif value == 'Bachelor of Secondary Education':
+        return 'BSEd'
+    elif value == 'Bachelor of Elementary Education':
+        return 'BEEd'
+    else:
+        return 'TBA'
